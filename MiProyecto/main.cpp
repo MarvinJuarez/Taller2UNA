ed88031dcd6e23dd646e85b173db085cc0974e03
ellioth <elliothjcc@hotmail.com>
2018-10-26 11:05:25 -0600

    main fue implementado

---
+++ MiProyecto/main.cpp
@@ -0,0 +1,26 @@
+int main(){ 
+	
+	int salir =0;
+int opc;
+
+while (salir==0){
+	cout<<"menu"<<endl
+		<<"1.Decir hola mundo"<<endl
+		<<"opc:";
+	
+	cin>> opc
+		
+		switch(opc){
+		case 1: 
+			break;
+			
+		default:
+			break;
+	}
+
+
+}	
+	
+	return 0;
+
+}
